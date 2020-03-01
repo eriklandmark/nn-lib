7373d8afafa516dc26b8570756c02ac64cf0c6c4
Erik Landmark <erik@landmark.se>
2020-03-01 14:33:00 +0100

    Tried with speech recognition

--- main.ts
+++ main.ts
@@ -1,41 +1,82 @@
-import Dataset from "./lib/dataset"
+import Dataset, {Example} from "./lib/dataset"
 import DenseLayer from "./lib/dense_layer";
 import OutputLayer from "./lib/output_layer";
 import Model from "./lib/model"
 import Activations from "./lib/activations";
+import * as fs from "fs";
+import * as path from "path";
+import Vector from "./lib/vector";
 
 let dataset = new Dataset();
 
-dataset.BATCH_SIZE = 1000
-dataset.loadMnistTrain("./dataset")
+dataset.BATCH_SIZE = 100
+// dataset.loadMnistTrain("./dataset")
+dataset.IS_GENERATOR = true
+
+const train_images: string[] = fs.readFileSync("./dataset/speech/train_list.txt", {encoding: "UTF-8"})
+    .trim().split("\n").map((s: string) => s.trim())
+
+dataset.TOTAL_EXAMPLES = train_images.length
+
+dataset.setGenerator(async (batch_id: number) => {
+    let examples: Example[] = []
+
+    for (let i = batch_id * dataset.BATCH_SIZE; i < batch_id*dataset.BATCH_SIZE + dataset.BATCH_SIZE; i++) {
+        const file = train_images[i].replace("wav", "png")
+        let label = -1;
+        if (file.startsWith("_background")) {
+            label = 0;
+        } else if (file.startsWith("down")) {
+            label = 1;
+        } else if (file.startsWith("left")) {
+            label = 2;
+        } else if (file.startsWith("right")) {
+            label = 3;
+        } else if (file.startsWith("up")) {
+            label = 4;
+        }
+
+        examples.push({
+            data: await Dataset.read_image(path.join("./dataset/speech", file)),
+            label: Vector.toCategorical(label, 5)
+        })
+    }
+
+    return examples
+})
 
 let layers = [
-    new DenseLayer(32, 28*28, Activations.sigmoid, Activations.sigmoid_derivative),
-    new DenseLayer(32, 32, Activations.sigmoid, Activations.sigmoid_derivative),
-    new DenseLayer(32, 32, Activations.sigmoid, Activations.sigmoid_derivative),
-    new OutputLayer(10, 32, Activations.Softmax, Activations.sigmoid_derivative)
+    new DenseLayer(64, 128*118, Activations.sigmoid, Activations.sigmoid_derivative),
+    new DenseLayer(32, 64, Activations.sigmoid, Activations.sigmoid_derivative),
+    new DenseLayer(24, 32, Activations.sigmoid, Activations.sigmoid_derivative),
+    new OutputLayer(5, 24, Activations.Softmax, Activations.sigmoid_derivative)
 ]
 
 let model = new Model(layers)
 
-model.train(dataset, 30, 0.0005)
+run()
 
-model.save("./nn.json")
+async function run() {
+    await model.train(dataset, 30, 0.0001)
 
-const testDataset = new Dataset();
-testDataset.loadMnistTest("dataset/test", 10000);
-testDataset.BATCH_SIZE = 10000
+    model.save("./nn.json")
 
-let examples = testDataset.getBatch(0)
+    /*const testDataset = new Dataset();
+    testDataset.loadMnistTest("dataset/test", 10000);
+    testDataset.BATCH_SIZE = 10000
 
-let numRights = 0;
+    let examples = testDataset.getBatch(0)
 
-for (let i = 0; i < testDataset.size(); i++ ) {
-    const predArg = model.predict(examples[i].data).argmax(0)
-    const labelArg = examples[i].label.argmax();
-    if (predArg == labelArg) {
-        numRights += 1
+    let numRights = 0;
+
+    for (let i = 0; i < testDataset.size(); i++ ) {
+        const predArg = model.predict(examples[i].data).argmax(0)
+        const labelArg = examples[i].label.argmax();
+        if (predArg == labelArg) {
+            numRights += 1
+        }
     }
+
+    console.log("Num rights: " + numRights + " of 10000 (" + Math.round((numRights / 10000) * 100) + " %)")*/
 }
 
-console.log("Num rights: " + numRights + " of 10000 (" + Math.round((numRights / 10000) * 100) + " %)")
