37b747f765f6c8e222aee59d4619d9c34296ee5a
Erik Landmark <erik@landmark.se>
2020-05-25 01:25:13 +0200

    build 1.4.3

--- src/model.ts
+++ src/model.ts
@@ -148,10 +148,13 @@ export default class Model {
             this.layers[i].useGpu = this.settings.USE_GPU
             this.layers[i].learning_rate = learning_rate
             if (i == this.layers.length - 1) {
-                (<OutputLayer>this.layers[i]).lossFunction = new lossFunction()
+                const lossFunc = new lossFunction()
+                (<OutputLayer>this.layers[i]).lossFunction = lossFunction
+                this.backlog.info.loss = lossFunc.name
             }
             this.layers[i].buildLayer(i == 0 ? inputShape : this.layers[i - 1].shape)
             this.layers[i].optimizer = new optimizer(this.layers[i])
+
         }
 
         if (verbose) {
@@ -163,6 +166,8 @@ export default class Model {
         this.backlog.info.total_neurons = this.backlog.info.model_structure.map((info) => info.shape).reduce((acc, val) => {
             return acc + val.reduce((a, s) => a * s, 1)
         }, 0)
+
+        this.backlog.info.optimizer = this.layers[0].optimizer.name
 
         this.isBuilt = true;
     }
